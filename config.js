33f127b03a73695ef19f0f8cc026392847533f21
unknown <thanabodee4301@gmail.com>
2019-06-25 01:49:58 +0700

    add link

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // https://discordapp.com/oauth2/authorize?&client_id=<CLIENT ID>&scope=bot&permissions=0
 // NTkyNDA1NTA3MDY3ODcxMjMy.XQ-84g.y93EmgRGpzq8gZvIwNcQnicqoDQ
+// https://discordapp.com/developers/applications/
